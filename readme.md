7fb948cd8c93b99f1e8653bc58dea28fda2d1bc3
airykka <okafor.erica@gmail.com>
2021-02-07 13:31:46 +0100

    New UI, JS Refactoring, Readme file

---
+++ readme.md
@@ -0,0 +1,31 @@
+# Lucky Numbers
+
+Lucky Numbers is a one-player game developed solely with vanilla JS, HTML & pure CSS. In the game, there is a secret number and the player is expected to guess what that number is based on the feedback generated from their previous guesses.
+
+## Quick Start/Installation
+
+<!-- prettier-ignore -->
+* Clone the repo
+
+  ```shell
+  git clone https://github.com/airykka/Lucky-Numbers.git
+  ```
+* Then enter the folder ```Lucky-Numbers```
+
+    ```shell
+  cd Lucky-Numbers
+  ```
+
+* Then open the ```index.html``` file in your web browser on your local machine.
+
+* ...and voila! You can now play the game.
+
+## Game Rules
+
+1. There is a hidden number behind the large square.
+2. Your goal is to guess what it is in 20 guesses or less.
+3. The number falls between 1 and 99.
+4. You lose the game when you run out of guesses.
+5. The `Play Again` button allows you to try and beat your highscore.
+
+> Happy Playing and I hope you enjoy the game as much as I did :)
--- script.js
+++ script.js
@@ -1,63 +1,72 @@
 'use strict';
-/*
-console.log(document.querySelector('.p1').textContent);
 
-document.querySelector('.p1').textContent = 'Correct Number!';
-document.querySelector('.qtn').textContent = '12';
-document.querySelector('.score').textContent = '15';
-document.querySelector('.input').value = '33';
+const feedbackDisplay = document.querySelector('.fback');
+const input = document.querySelector('.input');
+const hiddenNumberDisplay = document.querySelector('.qtn');
+const highScoreDisplay = document.querySelector('.highscore');
+const guessesLeftDisplay = document.querySelector('.guesses-left');
+const playAgainBtn = document.querySelector('.play-again');
+const guessBtn = document.querySelector('.check');
 
-console.log(document.querySelector('.input').value);
-*/
-
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 99) + 1;
 let score = 20; //state variable
 let highScore = 0;
 
 const displayMessage = function (message) {
-  document.querySelector('.p1').textContent = message;
+  feedbackDisplay.textContent = message;
 };
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.input').value);
-  console.log(guess);
-
-  //When there is no input
-  if (!guess) {
-    displayMessage('No Number!');
-
-    //When player wins
-  } else if (guess === secretNumber) {
-    displayMessage('Correct Number');
-    document.querySelector('.qtn').textContent = secretNumber;
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.qtn').style.width = '160px';
-
-    if (score > highScore) {
-      highScore = score;
-      document.querySelector('.highscore').textContent = highScore;
-    }
-
-    //When guess is wrong (too high or too low)
-  } else if (guess !== secretNumber) {
-    if (score > 1) {
-      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      displayMessage('Game Over!');
-      document.querySelector('.score').textContent = 0;
-    }
-  }
-});
-
-document.querySelector('.again').addEventListener('click', function () {
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+const init = function () {
+  secretNumber = Math.trunc(Math.random() * 99) + 1;
   score = 20;
-  document.querySelector('.score').textContent = score;
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.qtn').textContent = '?';
-  document.querySelector('.input').value = '';
+  guessesLeftDisplay.textContent = score;
+  hiddenNumberDisplay.textContent = '?';
+  hiddenNumberDisplay.style.backgroundColor = '#473567';
+  hiddenNumberDisplay.style.color = '#634d8b';
+  input.value = '';
+  guessBtn.addEventListener('click', guessBehaviour);
   displayMessage('Start Guessing...');
-  document.querySelector('.qtn').style.width = '80px';
-});
+};
+
+const guessBehaviour = function () {
+  if (input.value !== '') {
+    const guess = Number(input.value);
+
+    //When there is no input
+    if (guess <= 0) {
+      displayMessage('Invalid Guess! (1 - 99)');
+
+      //When player wins
+    } else if (guess === secretNumber) {
+      displayMessage('Correct Number :)');
+      hiddenNumberDisplay.textContent = secretNumber;
+      hiddenNumberDisplay.style.backgroundColor = '#60b347';
+      hiddenNumberDisplay.style.color = '#fff';
+
+      if (score > highScore) {
+        highScore = score;
+        highScoreDisplay.textContent = highScore;
+      }
+
+      guessBtn.removeEventListener('click', guessBehaviour);
+
+      //When guess is wrong (too high or too low)
+    } else if (guess !== secretNumber) {
+      if (score > 1) {
+        displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+        score--;
+        guessesLeftDisplay.textContent = score;
+      } else {
+        displayMessage('Game Over!');
+        guessesLeftDisplay.textContent = 0;
+      }
+    }
+    //When there is no input (ie.. user has not put in anything, The guess button should be unclickable)
+  } else {
+    displayMessage('No Number!');
+  }
+};
+
+guessBtn.addEventListener('click', guessBehaviour);
+
+playAgainBtn.addEventListener('click', init);
